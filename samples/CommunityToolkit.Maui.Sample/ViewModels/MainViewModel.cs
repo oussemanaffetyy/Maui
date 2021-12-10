7e7423309eb0cca516da1b111bfa290eeb604869
Vladislav Antonyuk <33021114+VladislavAntonyuk@users.noreply.github.com>
2021-12-10 22:57:40 +0200

    122 UniformGrid (#139)
    * 122 UniformGrid
    
    * Fix for rc2. remove compatibility usage
    
    * Add comments, add tests
    
    * Fix uniform tests
    
    * MaxRows, MaxColumns
    
    * Add samples
    
    Co-authored-by: Brandon Minnick <13558917+brminnick@users.noreply.github.com>
    Co-authored-by: Pedro Jesus <pedrojesus.cefet@gmail.com>

--- samples/CommunityToolkit.Maui.Sample/ViewModels/MainViewModel.cs
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/MainViewModel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Maui.Sample.Models;
 using CommunityToolkit.Maui.Sample.Pages.Alerts;
 using CommunityToolkit.Maui.Sample.Pages.Behaviors;
 using CommunityToolkit.Maui.Sample.Pages.Converters;
+using CommunityToolkit.Maui.Sample.Pages.Views;
 using CommunityToolkit.Maui.Sample.Pages.Extensions;
 using Microsoft.Maui.Graphics;
 
@@ -15,6 +16,12 @@ public class MainViewModel : BaseGalleryViewModel
 		new SectionModel(typeof(BehaviorsGalleryPage), "Behaviors", Color.FromArgb("#8E8CD8"),
 			"Behaviors lets you add functionality to user interface controls without having to subclass them. Behaviors are written in code and added to controls in XAML or code"),
 
+        new SectionModel(typeof(ConvertersGalleryPage), "Converters", Color.FromArgb("#EA005E"),
+            "Converters let you convert bindings of a certain type to a different value, based on custom logic"),
+
+        new SectionModel(typeof(ViewsGalleryPage), "Views", Color.FromArgb("#EA005E"),
+            "Views"),
+    };
 		new SectionModel(typeof(ConvertersGalleryPage), "Converters", Color.FromArgb("#EA005E"),
 			"Converters let you convert bindings of a certain type to a different value, based on custom logic"),
 
---
+++ src/CommunityToolkit.Maui.UnitTests/Views/UniformGrid_Tests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Maui.UnitTests.Mocks;
+using CommunityToolkit.Maui.Views;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using NSubstitute;
+using Xunit;
+
+namespace CommunityToolkit.Maui.UnitTests.Views;
+
+public class UniformGridTests : BaseTest
+{
+	UniformGrid uniformGrid;
+	IView uniformChild;
+	const double childCount = 3;
+	const double childWidth = 20;
+	const double childHeight = 10;
+	public UniformGridTests()
+	{
+		uniformChild = Substitute.For<IView>();
+		uniformGrid = new UniformGrid()
+		{
+			Children =
+			  {
+				uniformChild,
+				uniformChild,
+				uniformChild
+			  }
+		};
+	}
+
+	[Fact]
+	public void MeasureUniformGrid_NoWrap()
+	{
+		var expectedSize = new Size(childWidth * childCount, childHeight);
+		var childSize = new Size(childWidth, childHeight);
+		SetupChildrenSize(childSize);
+		var actualSize = uniformGrid.Measure(childWidth * childCount, childHeight * childCount);
+		Assert.Equal(expectedSize, actualSize);
+	}
+
+	[Fact]
+	public void MeasureUniformGrid_WrapOnNextRow()
+	{
+		var expectedSize = new Size(childWidth, childHeight * childCount);
+		var childSize = new Size(childWidth, childHeight);
+		SetupChildrenSize(childSize);
+		var actualSize = uniformGrid.Measure(childWidth, childHeight * childCount);
+		Assert.Equal(expectedSize, actualSize);
+	}
+
+	[Fact]
+	public void ArrangeChildrenUniformGrid()
+	{
+		var expectedSize = new Size(childWidth, childHeight);
+		SetupChildrenSize(expectedSize);
+		var actualSize = uniformGrid.ArrangeChildren(new Rectangle(0, 0, childWidth * childCount, childHeight * childCount));
+		Assert.Equal(expectedSize, actualSize);
+	}
+
+	[Fact]
+	public void MaxRowsArrangeChildrenUniformGrid()
+	{
+		var expectedSize = new Size(childWidth, childHeight);
+		SetupChildrenSize(expectedSize);
+		uniformGrid.MaxColumns = 1;
+		uniformGrid.MaxRows = 1;
+		var actualSize = uniformGrid.Measure(double.PositiveInfinity, double.PositiveInfinity);
+		Assert.Equal(expectedSize, actualSize);
+	}
+
+	void SetupChildrenSize(Size size)
+	{
+		uniformChild.Measure(double.PositiveInfinity, double.PositiveInfinity).ReturnsForAnyArgs(size);
+		uniformGrid.Measure(childWidth * childCount, childHeight * childCount);
+	}
+}
